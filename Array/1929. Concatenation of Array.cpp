88e6a22d4d54d00df8ed7c11c463e07355ae8f1e
Zubaer Haque <zubaer.haque@gmail.com>
2022-06-19 23:37:12 +0300

    runtime data added

--- Array/1929. Concatenation of Array.cpp
+++ Array/1929. Concatenation of Array.cpp
@@ -7,3 +7,5 @@ public:
 
     }
 };
+// Runtime: 10 ms, faster than 77.15% of C++ online submissions for Concatenation of Array.
+// Memory Usage: 12.9 MB, less than 38.76% of C++ online submissions for Concatenation of Array.
