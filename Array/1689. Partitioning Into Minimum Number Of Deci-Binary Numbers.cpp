b666b02c439119d7a029cff9e6d8a1871ce24c82
Zubaer Haque <zubaer.haque@gmail.com>
2022-06-22 23:56:24 +0300

    compledeted 1689

--- Array/1689. Partitioning Into Minimum Number Of Deci-Binary Numbers.cpp
+++ Array/1689. Partitioning Into Minimum Number Of Deci-Binary Numbers.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     int minPartitions(string n) {
-        
+        int highest = 0;
+        int temp = 0;
+        for(short i = 0; i<n.size(); i++)
+        {   
+            stringstream ss;
+            ss << n[i];
+            ss >> temp; 
+            if(highest < temp)
+                highest = temp;
+        }
+        return highest;
     }
 };
