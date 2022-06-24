87a10efb3438de2f21b4bd351cbd87434181369a
Zubaer Haque <zubaer.haque@gmail.com>
2022-06-24 14:00:05 +0300

    1689

---
+++ Array/1689..cpp
@@ -0,0 +1,14 @@
+#include <string>
+class Solution {
+public:
+    int minPartitions(string n) {
+        int temp=0;
+        sort( n.begin(),  n.end());
+        stringstream ss;
+            ss << n.back();
+            ss >> temp; 
+        return temp;
+    }
+};
+Runtime: 136 ms, faster than 5.18% of C++ online submissions for Partitioning Into Minimum Number Of Deci-Binary Numbers.
+Memory Usage: 13.6 MB, less than 17.88% of C++ online submissions for Partitioning Into Minimum Number Of Deci-Binary Numbers.
