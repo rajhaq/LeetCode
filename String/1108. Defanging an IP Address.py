8dae6f790c80e6e1f3c566db02a90325af1272a5
Zubaer Haque <zubaer.haque@4-cast.de>
2025-02-21 01:19:32 +0100

    1108 . Defanging an IP Address added

---
+++ String/1108. Defanging an IP Address.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def defangIPaddr(self, address):
+        """
+        :type address: str
+        :rtype: str
+        """
+        new_address = ""
+        for data in address:
+            if data == ".":
+                new_address += "[.]"
+            else:
+                new_address += data
+        return new_address
